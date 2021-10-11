b11f582c355dd8c6433b33d56136a71bedf43b57
Nureddin Elmas <nureddinelmas34@gmail.com>
2021-10-11 12:01:28 +0200

    Add Maps Activity!!

---
+++ app/src/main/java/com/se/iths/app21/grupp1/myapplication/Inloggning.kt
@@ -0,0 +1,64 @@
+package com.se.iths.app21.grupp1.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import com.se.iths.app21.grupp1.myapplication.databinding.ActivityInloggningBinding
+
+class Inloggning : AppCompatActivity() {
+    private lateinit var binding : ActivityInloggningBinding
+
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityInloggningBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+
+        val currentUser = auth.currentUser
+
+        if(currentUser != null){
+            val intent = Intent(this@Inloggning, MapsActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    fun signUpClicked(view: View){
+
+        val email = binding.emailNameText.text.toString()
+        val password = binding.passwordText.text.toString()
+
+        if (email.isNotEmpty() && password.isNotEmpty()){
+            auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener {
+                val intent = Intent(this@Inloggning, MapsActivity::class.java)
+                startActivity(intent)
+            }.addOnFailureListener {
+                Toast.makeText(this@Inloggning, it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        }else{
+            Toast.makeText(this@Inloggning, "Enter email and password", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun signInClicked(view: View){
+
+        val email = binding.emailNameText.text.toString()
+        val password = binding.passwordText.text.toString()
+
+        if(email.isNotEmpty() && password.isNotEmpty()){
+            auth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+                val intent = Intent(this@Inloggning, MapsActivity::class.java)
+                startActivity(intent)
+            }.addOnFailureListener {
+                Toast.makeText(this@Inloggning, it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+
+        }else{
+            Toast.makeText(this@Inloggning, "Enter email and password", Toast.LENGTH_LONG).show()
+        }
+    }
+}
