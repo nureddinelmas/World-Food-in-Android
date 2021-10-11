7691a5362d28e647518baab3e10fb9c095a608ca
Nureddin Elmas <nureddinelmas34@gmail.com>
2021-10-11 12:49:24 +0200

    Item Menu flyttat till Maps Activitet och rättat Main Activitet

--- app/src/main/java/com/se/iths/app21/grupp1/myapplication/MapsActivity.kt
+++ app/src/main/java/com/se/iths/app21/grupp1/myapplication/MapsActivity.kt
@@ -1,7 +1,10 @@
 package com.se.iths.app21.grupp1.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,6 +12,8 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 import com.se.iths.app21.grupp1.myapplication.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -16,9 +21,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        auth = FirebaseAuth.getInstance()
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -27,7 +35,29 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.place_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.signIn -> {
+                val intent = Intent(this@MapsActivity, Inloggning::class.java)
+                startActivity(intent)
+
+            }
+            R.id.signOut -> {
+                auth.signOut()
+            }
+            R.id.add_place -> {
+                val intent = Intent(this@MapsActivity, AddPlace::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
