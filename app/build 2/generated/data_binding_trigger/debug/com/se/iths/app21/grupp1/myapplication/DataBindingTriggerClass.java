3e05d079397a0269c469ce054c79a994e9cadb1d
sannajohansson <susanna.johansson@iths.se>
2021-10-28 17:36:05 +0200

    PlacesActivity

---
+++ app/build 2/generated/data_binding_trigger/debug/com/se/iths/app21/grupp1/myapplication/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.se.iths.app21.grupp1.myapplication;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
---
+++ app/src/main/java/com/se/iths/app21/grupp1/myapplication/Comments.kt
@@ -0,0 +1,7 @@
+package com.se.iths.app21.grupp1.myapplication
+
+class Comments (
+
+    var comments: String? = null
+
+        )
